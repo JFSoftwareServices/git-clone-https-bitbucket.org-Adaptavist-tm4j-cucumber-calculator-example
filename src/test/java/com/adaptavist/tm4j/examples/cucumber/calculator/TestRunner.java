ab9e84eac57a814d92c0550036f23bca37dd9ce8
jidefakoya <jidefakoya@hotmail.co.uk>
2020-04-18 20:00:03 +0100

    amending test runner

--- src/test/java/com/adaptavist/tm4j/examples/cucumber/calculator/TestRunner.java
+++ src/test/java/com/adaptavist/tm4j/examples/cucumber/calculator/TestRunner.java
@@ -7,9 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         strict = true,
-        plugin = {"pretty", "html:target/cucumber", "junit:target/cucumber.xml"},
-        features = "classpath:features",
-//        features = "src/test/resources/features"
+        plugin = {"pretty", "html:target/cucumber", "junit:target/cucumber.xml", "json:target/cucumber/calculator.json"},
+//        features = "classpath:features",
+        features = "src/test/resources/features",
         glue = {"com/adaptavist/tm4j/examples/cucumber/calculator"}
 )
 public class TestRunner {
